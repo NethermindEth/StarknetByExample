4a80e1c671468788265b87a426e570fbed5bf753
julienbrs <julien.bourseau@gmail.com>
2023-11-17 16:12:03 +0100

    ci: asynchronous + only modified files

--- scripts/cairo_programs_verifier.sh
+++ scripts/cairo_programs_verifier.sh
@@ -7,34 +7,36 @@ NC='\033[0m'
 # flag to check if any errors were encountered
 has_errors=false
 
-# function to process directory
-process_directory() {
-  for dir in "$1"/*
-  do
-    if [ -d "$dir" ]; then
-      echo "Processing $dir"
-      (cd "$dir" && scarb build 0>/dev/null 1> error.log && scarb fmt -c 0>/dev/null 1>> error.log && scarb test 0>/dev/null 1>> error.log)
-      if [ $? -ne 0 ]; then
-        has_errors=true
-        echo "Error while processing $dir"
-        cat "$dir/error.log"
-      fi
-      rm "$dir/error.log"
-    fi
-  done
+# function to list modified cairo files
+list_modified_cairo_files() {
+    git diff --name-only master...HEAD -- listings | grep -E 'listings/ch.*/*.cairo$'
 }
 
-# only start processing directories that match the pattern ch*-* 
-for dir in listings/ch*-*
-do
-  if [ -d "$dir" ]; then
-    process_directory "$dir"
-  fi
+# function to process individual file
+process_file() {
+    dir=$(dirname "$1")
+    file=$(basename "$1")
+    echo "Processing $dir/$file"
+    (cd "$dir" && scarb build "$file" 0>/dev/null 1> error.log && scarb fmt -c "$file" 0>/dev/null 1>> error.log && scarb test "$file" 0>/dev/null 1>> error.log)
+    if [ $? -ne 0 ]; then
+        has_errors=true
+        echo "Error while processing $dir/$file"
+        cat "$dir/error.log"
+    fi
+    rm "$dir/error.log"
+}
+
+# process each modified file
+modified_files=$(list_modified_cairo_files)
+for file in $modified_files; do
+    process_file "$file" &
 done
+
+wait  # Wait for all background processes to finish
 
 # check if any errors were encountered
 if $has_errors ; then
-  echo "\n${RED}Some projects have errors, please check the list above.${NC}\n"
+  echo -e "\n${RED}Some projects have errors, please check the list above.${NC}\n"
   exit 1
 else
   echo -e "\n${GREEN}All scarb builds were completed successfully${NC}.\n"
