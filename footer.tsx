1ce536b14890d37ac397acab666c7f998764bdc7
julio4 <30329843+julio4@users.noreply.github.com>
2024-11-03 00:41:46 +0700

    feat: footer

---
+++ footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+      <div className="flex flex-col items-center pb-2 gap-2">
+        <img className="w-40" src="/collaborators/PoweredByNethermind.svg" alt="Powered By Nethermind"/>
+        <div className="footer_text text-xs flex flex-col items-center">
+          <span>Released under the MIT License.</span>
+          <span>© 2024 Nethermind. All Rights Reserved</span>
+        </div>
+      </div>
+  )
+}
